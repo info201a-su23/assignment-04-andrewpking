9dfc39167256ce4a6e03e428997a819256c44323
Drew King <andrew.phillip.king@gmail.com>
2023-07-31 14:29:32 -0700

    Turned chart1 into a function for use with rmd

--- chart1_example.R
+++ chart1_example.R
@@ -2,27 +2,27 @@ source("summary.R")
 library(ggplot2)
 
 plot_inc_by_race <- function(){
+  # Load data and unpack it:
+  inc_tables <- load_data()
+  prison_pop <- inc_tables[[1]]
+  prison_jail_rate_1990 <- inc_tables[[3]]
   
-}
-# Load data and unpack it:
-inc_tables <- load_data()
-prison_pop <- inc_tables[[1]]
-prison_jail_rate_1990 <- inc_tables[[3]]
-
-# Create time series data for incarceration by race
-inc_by_race <- inc_rate_by_race(prison_jail = prison_jail_rate_1990)
-
-inc_by_race_plot <- ggplot(data = inc_by_race) +
-  geom_line(aes(x = year, y = population, 
-                colour = race, linetype = incarceration_type)) +
-  scale_x_continuous(breaks = seq(1990,2020,2), limits = c(1990, 2018)) +
-  scale_y_continuous(
-    breaks = seq(0, 8000000, 1000000), limits = c(0, 8000000)
+  # Create time series data for incarceration by race
+  inc_by_race <- inc_rate_by_race(prison_jail = prison_jail_rate_1990)
+  
+  # Plot the data in a human readable form:
+  inc_by_race_plot <- ggplot(data = inc_by_race) +
+    geom_line(aes(x = year, y = population, 
+                  colour = race, linetype = incarceration_type)) +
+    scale_x_continuous(breaks = seq(1990,2020,2), limits = c(1990, 2018)) +
+    scale_y_continuous(
+      breaks = seq(0, 8000000, 1000000), limits = c(0, 8000000)
     ) +
-  labs(title = "Incarceration in USA by Race", 
-       subtitle = "Aggregated from annual county incarceration data", 
-       x = "Year", y = "Population") +
-  guides(colour = guide_legend("Race"),
-         linetype = guide_legend("Incarceration Type"),
-         x = guide_axis(angle = 90))
-inc_by_race_plot  
+    labs(title = "Incarceration in USA by Race", 
+         subtitle = "Aggregated from annual county incarceration data", 
+         x = "Year", y = "Population") +
+    guides(colour = guide_legend("Race"),
+           linetype = guide_legend("Incarceration Type"),
+           x = guide_axis(angle = 90))
+  return(inc_by_race_plot)
+}
