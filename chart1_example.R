2b4b447101febba28a09c73b8cc1f8b032712b57
Drew King <andrew.phillip.king@gmail.com>
2023-07-31 13:54:22 -0700

    Fixed typo in time series graph labels.

--- chart1_example.R
+++ chart1_example.R
@@ -13,8 +13,8 @@ inc_by_race <- inc_rate_by_race(prison_jail = prison_jail_rate_1990)
 ggplot(data = inc_by_race) +
   geom_line(aes(x = year, y = population, 
                 colour = race, linetype = incarceration_type)) +
-  labs(title = "Inceration in USA by Race", 
-       subtitle = "Aggregated from county incarceration data", 
+  labs(title = "Incarceration in USA by Race", 
+       subtitle = "Aggregated from annual county incarceration data", 
        x = "Year", y = "Population") +
   guides(colour = guide_legend("Race"),
          linetype = guide_legend("Incarceration Type"))
