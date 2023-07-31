cf45f7b1848bb2a700a0f8f09e71d14aef71e7ea
Drew King <andrew.phillip.king@gmail.com>
2023-07-31 14:17:08 -0700

    Updated x-axis to include more years

--- chart1_example.R
+++ chart1_example.R
@@ -1,20 +1,25 @@
 source("summary.R")
 library(ggplot2)
 
+plot_inc_by_race <- function(){
+  
+}
 # Load data and unpack it:
 inc_tables <- load_data()
 prison_pop <- inc_tables[[1]]
-jail_pop <- inc_tables[[2]]
 prison_jail_rate_1990 <- inc_tables[[3]]
 
 # Create time series data for incarceration by race
 inc_by_race <- inc_rate_by_race(prison_jail = prison_jail_rate_1990)
 
-ggplot(data = inc_by_race) +
+inc_by_race_plot <- ggplot(data = inc_by_race) +
   geom_line(aes(x = year, y = population, 
                 colour = race, linetype = incarceration_type)) +
+  scale_x_continuous(breaks = seq(1990,2020,2), limits = c(1990, 2018)) +
   labs(title = "Incarceration in USA by Race", 
        subtitle = "Aggregated from annual county incarceration data", 
        x = "Year", y = "Population") +
   guides(colour = guide_legend("Race"),
-         linetype = guide_legend("Incarceration Type"))
+         linetype = guide_legend("Incarceration Type"),
+         x = guide_axis(angle = 90))
+inc_by_race_plot  
