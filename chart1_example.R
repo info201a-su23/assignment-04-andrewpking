e75dbf62f043874370888cbd6dcb536daed62f2c
Drew King <andrew.phillip.king@gmail.com>
2023-07-31 14:25:00 -0700

    Updated y axis scaling in chart1

--- chart1_example.R
+++ chart1_example.R
@@ -16,6 +16,9 @@ inc_by_race_plot <- ggplot(data = inc_by_race) +
   geom_line(aes(x = year, y = population, 
                 colour = race, linetype = incarceration_type)) +
   scale_x_continuous(breaks = seq(1990,2020,2), limits = c(1990, 2018)) +
+  scale_y_continuous(
+    breaks = seq(0, 8000000, 1000000), limits = c(0, 8000000)
+    ) +
   labs(title = "Incarceration in USA by Race", 
        subtitle = "Aggregated from annual county incarceration data", 
        x = "Year", y = "Population") +
